ee41bbb86cd336be41a48cb1714248ba27911c41
E7G <55611737+E7G@users.noreply.github.com>
2026-09-07 22:22:26 +0800

    build: mark API37 safe test version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     val buildTime = System.currentTimeMillis()
-    val baseVersionName = "Preview1.1"
+    val baseVersionName = "Preview1.1-api37safe1"
     namespace = "com.buildsession.betterYAMF"
     compileSdk = 36
 
@@ -19,7 +19,7 @@ android {
         applicationId = "com.buildsession.betterYAMF"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = baseVersionName
 
         ndk {
